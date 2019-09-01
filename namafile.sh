89174f23400efa296866bf7163d8480489483502
zayen saiklen <54509123+zayensaiclon@users.noreply.github.com>
2019-09-02 01:42:39 +0700

    Add files via upload
    Jangan asal comot ya tod 
    Di dahulu kan izin dulu ama yg punya
    Gk izin gk barokah😎

---
+++ namafile.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+#version 1.0
+
+# Variables
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+
+
+figlet Tools | lolcat
+
+printf $red         " ✶▂▂▂▂☬☆✶☆☬▂▂▂▂✶"
+printf $red       " ╔⍝═☬═╝╭⍝☬⍝╮╚═☬═⍝╗"
+printf $red        " ║╔█✪⚞☬LOSARIE☬⚟✪ ╗"
+printf $red        " ✪║╚█⚞ANONYMOUSE⚟✪ ║"
+printf $red        " ╚══✇═╗╰✪☬✪╯╔═✇══╝ "
+
+
+echo $blue"-------------------------------------->"
+printf $blue"AUTOUR  :by.zayen saiklen"
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :kumpulan skrip "
+echo $red"----------------------------------------"
+# menu utama
+
+
+echo $green"[1].Toools tuan badutv1"|lolcat
+printf $red "==========================================#"
+echo $cyan"[2].Tools tuan badutv4"|lolcat
+printf $red "==========================================#"
+echo $white"[3].Tools B4j1n64nv5"|lolcat
+printf $red "==========================================#"
+echo $green"{4}.fb premium"|lolcat
+printf $red "===================================#"
+echo $yellow"{5}.Ddos Attack (Trojan)"|lolcat
+printf $red"====================================#"
+echo $red "___________________(WELCOME)_______________________>"
+echo "≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈™"
+echo $red "\033[32;1m╭──=>>[PILIH NOMORNYA]"
+read -p "╰───────────────> : " bos
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pjg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+cd TOOLSINSTALLERv1
+chmod +x Tuanb4dut.sh
+sh Tuanb4dut.sh
+fi
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+cd TOOLSINSTALLERv4
+ls
+chmod 777 TUANB4DUT..sh
+./TUANB4DUT..sh
+fi
+
+if [ $bos = 3 ] || [ $bos = 3 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install git
+pkg insatll toilet
+pkg install figlet
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg install git python2
+pip2 install --upgrade pip
+git clone https://github.com/riskiyana/PremiumFb
+cd PremiumFb
+pip2 install -r requirements.txt
+python2 PremiumFb.py
+fi
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+then
+clear
+figlet -f slant "Loading . . " | lolcat
+pkg update && pkg upgrade
+pkg install python
+pkg install bash
+pkg install toilet
+pkg install lolcat
+pkg install git
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+sh Tamfan-Ddos.sh
+fi
+exit
